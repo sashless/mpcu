cddf941834bfef28598546f426999c85dc32e021
sashless <saweniger@gmail.com>
2013-09-30 18:58:28 +0200

    refactored routes and form creation logic in homecontroller

--- src/Less/MpcuBundle/Controller/HomeController.php
+++ src/Less/MpcuBundle/Controller/HomeController.php
@@ -11,45 +11,50 @@ use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends Controller {
 	public function indexAction() {
-		return $this->render ( 'LessMpcuBundle:Home:index.html.twig', array (
-				'form_choose_role' => $this->setupFormChooseRole ()->createView () 
-		) );
+		return $this->render ( 'LessMpcuBundle:Home:index.html.twig', array(
+				'form_sender' => $this->setupChooseSenderForm()->createView(),
+				'form_reciever' => $this->setupChooseRecieverForm()->createView()));
 	}
-	private function setupFormChooseRole() {
-		$builder = $this->createFormBuilder ();
-		$builder->add ( 'Sender', 'submit' )->add ( 'Reciever', 'submit' )->setAction ( $this->generateUrl ( 'less_mpcu_home_choose' ) );
-		return $builder->getForm ();
+	private function setupChooseSenderForm(){
+		$builder_sender = $this->createFormBuilder ();
+		$builder_sender->add ( 'Sender', 'submit' )->setAction($this->generateUrl('less_mpcu_home_sender'));
+		
+		return $builder_sender->getForm();
 	}
-	public function chooseAction(Request $req) {
-		$now = new \DateTime ( 'now' );
+	private function setupChooseRecieverForm(){
+		$builder_reciever = $this->createFormBuilder ();
+		$builder_reciever->add ( 'Reciever', 'submit' )->setAction($this->generateUrl('less_mpcu_home_reciever'));
 		
-		$form = $this->setupFormChooseRole ();
-		$form->handleRequest ( $req );
-		
+		return $builder_reciever->getForm();
+	}
+	
+	public function senderAction(Request $req){
+		$form = $this->setupChooseSenderForm();
+		$form->handleRequest($req);
+	
 		if ($form->isValid ()) {
-			if ($form->get ( 'Sender' )->isClicked ()) {
 				return $this->render ( 'LessMpcuBundle:Home:sender.html.twig', array (
 						'ajax_settings' => urlencode ( json_encode ( array (
 								'ajax' => array (
 										'method' => 'GET',
-										'url' => $this->generateUrl ( 'less_mpcu_ajax_save' ) 
-								) 
-						) ) ) 
-				) );
-			} else {
-				return $this->render ( 'LessMpcuBundle:Home:reciever.html.twig', array (
-						'ajax_settings' => urlencode ( json_encode ( array (
-								'ajax' => array (
-										'method' => 'GET',
-										'url' => $this->generateUrl ( 'less_mpcu_ajax_get_next')
-    						),
-    						'initTime' => $now->getTimestamp()
-    				)
-    			))));
-    		}
-    	}else{
-    		return $this->redirect($this->generateUrl('less_mpcu_home'));
-    	}
-    }
-    
+										'url' => $this->generateUrl ( 'less_mpcu_ajax_save' )
+								)
+				)))));
+		}
+	}
+	public function recieverAction(Request $req){
+		$form = $this->setupChooseRecieverForm();
+		$form->handleRequest($req);
+		$now = new \DateTime ( 'now' );
+		if ($form->isValid ()) {
+			return $this->render ( 'LessMpcuBundle:Home:reciever.html.twig', array (
+					'ajax_settings' => urlencode ( json_encode ( array (
+							'ajax' => array (
+									'method' => 'GET',
+									'url' => $this->generateUrl ( 'less_mpcu_ajax_get_next' )
+							),
+							'initTime' => $now->getTimestamp()
+					)))));
+		}			
+	}
 }
